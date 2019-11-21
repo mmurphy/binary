3b014a7c9ca697c2a63c0f1a20427747295d70c4
Martin Murphy <mmurphy@redhat.com>
2019-11-21 18:21:56 +0000

    fix listen port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var app = express();
