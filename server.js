35cb163d569fff3912e6d0927629e250413c2c2a
Martin Murphy <mmurphy@redhat.com>
2019-11-21 18:14:45 +0000

    initial server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var app = express();
+
+app.get('/', function (req,res){
+    console.log('received GET /');
+    res.send("hello");
+});
+
+app.listen(port,ip,function(){
+    console.log('listening on %s:%d', ip, port);
+})
